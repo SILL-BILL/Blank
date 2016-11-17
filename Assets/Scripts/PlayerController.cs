e38185399f5605e4366ab7cdba43e5ecd3ec5422
SILL-BILL <7th5jack@gmail.com>
2016-11-17 11:09:27 +0900

    update main scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	private CharacterController controller;
+	private Animator animator;
+	private Vector3 moveDirection = Vector3.zero;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		controller = GetComponent<CharacterController>();
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+
+	public void moveToLeft(){
+
+	}
+
+	public void moveToRight(){
+
 	}
 }
